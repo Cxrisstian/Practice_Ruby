09100233d2ade8ce50795836e1d215952dffbf5f
Cristian Lujan Villca <Cristian Lujan@jalasoft.local>
2019-10-11 18:32:53 -0400

    the firs practice

---
+++ Practices/Practice02.rb
@@ -0,0 +1,11 @@
+=begin
+@Cxristian
+This method show the sum
+only with two numbers
+=end
+puts "Enter 1st number : "
+number1 = gets.to_i
+puts "Enter 2do number : "
+number2 = gets.to_i
+puts "the sum total is #{number1 + number2 + number3}"
+"I think that value \#{}\ serves to perform an action inside a string literal.."
