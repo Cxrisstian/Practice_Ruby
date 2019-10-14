f58961fe2b02563f25f4a0d17c0353eb25f0244a
Cristian Lujan <cristian.lujan@jalasoft.com>
2019-10-14 19:44:27 -0400

    add more classes

---
+++ Practices/practice000.rb
@@ -0,0 +1,19 @@
+module Week
+  FIRST_DAY = "Sunday"
+  def Week.weeks_in_month
+    puts "You have four weeks in a month"
+  end
+  def Week.weeks_in_year
+    puts "You have 52 weeks in a year"
+  end
+end
+puts Week::FIRST_DAY
+Week.weeks_in_month
+Week.weeks_in_year
+
+module Greets
+  def Greets.hello
+    puts "Hi Friend"
+  end
+  def Greets.
+end
